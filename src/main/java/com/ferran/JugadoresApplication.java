ca5271b42d5c53c2f96cf1079dea606268e45be6
Ferran Torredeflot <ftorredeflot@gmail.com>
2016-10-17 20:37:08 +0200

    Initial commit

---
+++ src/main/java/com/ferran/JugadoresApplication.java
@@ -0,0 +1,12 @@
+package com.ferran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JugadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JugadoresApplication.class, args);
+	}
+}
---
+++ src/main/java/com/ferran/domain/Jugador.java
@@ -0,0 +1,10 @@
+package com.ferran.domain;
+import javax.persistence.*;
+/**
+ * Created by ferran on 17/10/16.
+ */
+@Entity
+public class Jugador {
+     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+}
