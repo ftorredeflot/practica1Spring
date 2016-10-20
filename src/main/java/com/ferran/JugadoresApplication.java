e1592aa13a10392b805416740f0e904e7e2d1a49
Ferran Torredeflot <ftorredeflot@gmail.com>
2016-10-20 19:19:42 +0200

    spring boot

--- src/main/java/com/ferran/JugadoresApplication.java
+++ src/main/java/com/ferran/JugadoresApplication.java
@@ -1,12 +1,18 @@
 package com.ferran;
 
+import com.ferran.service.PlayerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class JugadoresApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JugadoresApplication.class, args);
+
+		ConfigurableApplicationContext context= SpringApplication.run(JugadoresApplication.class, args);
+		context.getBean(PlayerService.class).testPlayers();
+
+
 	}
 }
