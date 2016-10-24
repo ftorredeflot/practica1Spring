8ccee3e7997a9c5c739c058d22a1f721c270875c
Ferran Torredeflot <ftorredeflot@gmail.com>
2016-10-24 20:59:59 +0200

    semilla de controler

---
+++ src/main/java/com/ferran/controller/PlayerController.java
@@ -0,0 +1,32 @@
+package com.ferran.controller;
+
+import com.ferran.domain.Player;
+import com.ferran.repository.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by ferran on 24/10/16.
+ */
+@RestController
+@RequestMapping("/players")
+public class PlayerController {
+
+    @Autowired
+    private PlayerRepository playerRepository;
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Player createPlayer(@RequestBody Player player){
+        return playerRepository.save(player);
+
+    }
+    @RequestMapping(method =RequestMethod.GET)
+    public List<Player> findAll(){
+        return playerRepository.findAll();
+    }
+}
